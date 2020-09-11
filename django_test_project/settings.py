acce7013c4da7d0a50d5f42ec334bf396829b8e1
Maueez Ahmed <maueezahmed@gmail.com>
2020-09-11 21:35:27 +0500

    protect secret key

--- django_test_project/settings.py
+++ django_test_project/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pm2=0owspm0@wd7n+#07=im!x-gtbren)l_!%1h!+xs$!a+wko'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
